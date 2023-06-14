790d2392e644fdd1726ebbe469b068bfe65da147
abranico <abranico011@gmail.com>
2023-06-14 18:04:15 -0300

    creacion de archivo C y declaracion de algunas variables y funciones

---
+++ CajeroAutomatico.c
@@ -0,0 +1,38 @@
+// INTEGRANTES:
+// - Valentino Maffeis
+// - Ivan Apanowicz
+// - Nicolas Abramovich
+// - Ramiro Muñoz
+// - Kevin Rodrigo Kener
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void iniciarSesion();
+void depositarSaldo();
+void retirarSaldo();
+void consultarSaldo();
+void mostrarOperacionesYSaldo();
+void cerrarSesion();
+
+void main()
+{
+    int cantClientes = 10;
+    int nroCuentaCliente;
+    char contraseñaCliente[20];
+    char nombreCliente[20];
+    float saldoCliente = 0;
+    char estadoCliente; // [Activo/Bloqueado]
+}
+
+void iniciarSesion() {}
+
+void depositarSaldo() {}
+
+void retirarSaldo() {}
+
+void consultarSaldo() {}
+
+void mostrarOperacionesYSaldo() {}
+
+void cerrarSesion() {}
